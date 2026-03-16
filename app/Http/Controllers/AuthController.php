4c5ebf17f8b6ba1cbc4716eea9eef5e546111f2e
lakroune <lakroune00@gmail.com>
2026-03-16 15:15:15 +0000

    feat: add laravel/sanctum package for API authentication and update validation rules in RegisterRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginResquest;
 use App\Http\Requests\RegisterResquest;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -13,18 +12,14 @@ class AuthController extends Controller
     public function register(RegisterResquest $request)
     {
         $data = $request->validated();
-        if ($data->fails()) {
-            return response()->json($data->errors(), 400);
-        }
+
         $user =  User::created($data);
         return response()->json($user, 201);
     }
     public  function login(LoginResquest $request)
     {
         $data = $request->validated();
-        if ($data->fails()) {
-            return response()->json($data->errors(), 400);
-        }
+
         if (!$token = auth("api")->attempt($data)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
--- app/Http/Requests/RegisterResquest.php
+++ app/Http/Requests/RegisterResquest.php
@@ -23,9 +23,9 @@ class RegisterResquest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'unique:users,email'],
-            'password' => ['required', 'string', 'min:8'],
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
         ];
     }
 
