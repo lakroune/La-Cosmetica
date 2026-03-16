0701952ed3bf9cbf86c2a7da5ad8f802cdde1455
lakroune <lakroune00@gmail.com>
2026-03-16 11:50:15 +0000

    feat: define order_products table structure with foreign keys and quantity details

--- database/migrations/2026_03_16_114851_create_order_products_table.php
+++ database/migrations/2026_03_16_114851_create_order_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('order_products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('unit_price', 8, 2);
             $table->timestamps();
         });
     }
