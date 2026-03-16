6ee8ed2028313f43cd6511982af9cda16bb26eb1
lakroune <lakroune00@gmail.com>
2026-03-16 11:31:57 +0000

    feat: establish relationships between Category and Product models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,4 +11,8 @@ class Category extends Model
     use HasFactory;
     protected $fillable = ['name', 'description'];
     protected $table = 'categories';
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,10 @@ class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+    protected $fillable = ['name', 'description', 'price', 'stock', 'category_id'];
+    protected $table = 'products';
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
