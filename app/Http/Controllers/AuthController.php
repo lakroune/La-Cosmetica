ec6667602885ddf9ead29112ef7b57807750372e
lakroune <lakroune00@gmail.com>
2026-03-16 14:54:42 +0000

    feat: enhance registration and login methods in AuthController with validation and error handling

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,21 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginResquest;
 use App\Http\Requests\RegisterResquest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
 
-    public function register(RegisterResquest $request) {}
+    public function register(RegisterResquest $request)
+    {
+        $data = $request->validated();
+        if ($data->fails()) {
+            return response()->json($data->errors(), 400);
+        }
+        $user =  User::created($data);
+        return response()->json($user, 201);
+    }
     public  function login(LoginResquest $request)
     {
-
         $data = $request->validated();
-        $user = auth()->attempt($data);
-        if (!$user) {
-            return response()->json(['message' => 'Invalid credentials'], 401);
+        if ($data->fails()) {
+            return response()->json($data->errors(), 400);
         }
-
-        return response()->json(['message' => 'Login successful']);
+        if (!$token = auth("api")->attempt($data)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        return response()->json(['token' => $token], 200);
     }
 }
