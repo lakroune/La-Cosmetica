37343e0f6d95d643cf63669cdbe1cbe55cdc4ae5
lakroune <lakroune00@gmail.com>
2026-03-16 11:58:05 +0000

    refactor: update UserFactory to use $this->faker and optimize password assignment; add UserSeeder to DatabaseSeeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ?: (static::$password = Hash::make('password')),
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             RoleSeeder::class,
+            UserSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
             OrderSeeder::class,
