e8ca1ec9c93a1600a1f797042508c9fbcc4ad1b3
lakroune <lakroune00@gmail.com>
2026-03-23 13:02:55 +0100

    feat: enhance CategoryDTO with constructor for direct property assignment, add fromRequest method for instance creation from request data, and implement toArray method for improved data serialization

--- app/DTOs/CategoryDTO.php
+++ app/DTOs/CategoryDTO.php
@@ -7,8 +7,37 @@ class CategoryDTO
     /**
      * Create a new class instance.
      */
-    public function __construct()
-    {
+    public function __construct(
+        public readonly string $name,
+        public readonly ?string $description
+    ) {
         //
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $request
+     * @return self
+     */
+    public static function fromRequest($request): self
+    {
+        return new self(
+            name: $request->validated('name'),
+            description: $request->validated('description')
+        );
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+        ];
     }
 }
